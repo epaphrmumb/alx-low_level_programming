f44b91577d2e94ffd85d048c4d56d54db04d0ab2
epaphrmumb <epaphroditusmumbah@gmail.com>
2022-12-08 03:54:57 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
- *main - Entry point
- *
- *Return: Always 0 (success)
+ * main - Entry point
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	puts("\"progamming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }	
+		
