4009c05197cf809d7ace5edd2075aacd422392fe
epaphrmumb <epaphroditusmumbah@gmail.com>
2022-12-13 05:20:31 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_x10 - Print alphabet x10, in lowercase followed by a new line
  * Return: Always 0.
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 
 {
 	int count;
@@ -12,7 +12,9 @@ void print_alphabet_x10(void);
 	for (count = 0; count <= 9 ; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
+		}
 		_putchar('\n');
 	}
 }
