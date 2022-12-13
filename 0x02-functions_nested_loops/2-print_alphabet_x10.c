90c95797aad124ac5a48f5debc8c2c0a34bbaa74
epaphrmumb <epaphroditusmumbah@gmail.com>
2022-12-13 04:38:49 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,13 @@
 void print_alphabet_x10(void)
 
 {
-
 	int count;
 	char letter;
 
 	for (count = 0; count <= 9 ; count++)
 	{
-	for (letter = 'a'; letter <= 'z'; letter++)
-		{
-		_putchar(letter);
-		}
-	_putchar('\n');
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
 	}
 }
