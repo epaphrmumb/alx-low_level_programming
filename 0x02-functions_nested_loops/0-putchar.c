06ea8b4cdd7f8683cb0d5351ecf12272a162966f
epaphrmumb <epaphroditusmumbah@gmail.com>
2022-12-13 02:40:54 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - prints the string "Holberton" from a character array.
  * Return: 0 on success.
  */
 int main(void)
 {
-	char c[] = "_putchar";
+	char c[] = "Holberton";
 	int i;
 
 	for (i = 0; i < 9; i++)
