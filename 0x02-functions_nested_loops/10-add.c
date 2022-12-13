309f51b25a01f0141e892595cd274b63199296cb
epaphrmumb <epaphroditusmumbah@gmail.com>
2022-12-13 10:10:10 -0800

    98 Battery Street

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * add -  sum between two numbers
- * @a : number one
- * @b : number two
- * Return : sum
+ * add - sum between two numbers
+ * @num1 : number one
+ * @num2 : number two
+ * Return : sum of a and b
  **/
-int add(int a, int b)
+
+int add(int num1, int num2)
 {
-	return (a + b);
+	return (num1 + num2);
 }
