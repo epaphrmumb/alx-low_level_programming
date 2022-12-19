818f21e3dbce73975dbd2ea073bc698e6cfc5fb7
epaphrmumb <epaphroditusmumbah@gmail.com>
2022-12-19 04:00:29 -0800

    Engineer

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -1,19 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
+ *
  * Return: Always 0.
  */
-
 int main(void)
-
 {
-
-	char s[10] = "Holberton";
+	char s[10] = "My School";
 
 	printf("%s\n", s);
 	rev_string(s);
 	printf("%s\n", s);
-
 	return (0);
 }
