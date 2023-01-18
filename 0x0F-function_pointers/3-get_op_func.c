b096e11a267c09afb8b01c33604a003e760f487b
epahrmumb <epaphroditusmumbah@gmail.com>
2023-01-18 11:28:41 -0800

    All Tasks

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,39 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <string.h>
+
 /**
- * get_op_func - ...
- * @s: ...
- * Return: ...
+ * get_op_func - function that selects the correct function to perform the
+ *               operation asked by the user.
+ * @s: This is the input operator
+ * Return: This function returns a pointer to the function that corresponds to
+ *         the operator given as a parameter. Example: get_op_func("+")
+ *         should return a pointer to the function op_add"")
+ *         If s does not match any of the 5 expected operators
+ *         (+, -, *, /, %),return NULL)
  */
 
-int (*get_op_func(char *s))(int, int)
+int (*get_op_func(char *s))(int a, int b)
 
 {
+
 	op_t ops[] = {
-		
-		{ "+", op_add },
-		{ "-", op_sub },
-		{ "*", op_mul },
-		{ "/", op_div }
-		{ "%", op_mod },
-			{ NULL, NULL }
-			};
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 	int i = 0;
 
-	while (i < 5)
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
+		if (*ops[i].op == *s)
+		{
+			return (ops[i].f);
+		}
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
